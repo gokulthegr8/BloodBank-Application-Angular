f038083c860148c48e5bf57f5e95974e6c52ce1c
Gokul <gokull.mani@outlook.com>
2019-12-04 17:58:00 -0500

    finalCommit

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -16,7 +16,10 @@ app.use(cors())
 
 db.query('CREATE TABLE IF NOT EXISTS hospital_sign_up(`id` int(10) NOT NULL primary key AUTO_INCREMENT,`hospitalName` varchar(50) NOT NULL,`address` varchar(100) NOT NULL,`city` varchar(25) NOT NULL,`state` varchar(25) NOT NULL,`zipcode` int(7) NOT NULL,`hospitalId` int(15) NOT NULL UNIQUE,`password` varchar(255) NOT NULL)');
 // db.query('CREATE TABLE IF NOT EXISTS `hospital_sign_in` (`id` int(10) NOT NULL,`hospitalId` int(15) NOT NULL,`password` varchar(255) NOT NULL)')
-
+db.query('CREATE TABLE IF NOT EXISTS ordersNew (OrderID int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,HospitalID int(11) NOT NULL,BloodGroup varchar(255) NOT NULL,Quantity int(11) NOT NULL,Urgency varchar(255) NOT NULL)');
+db.query('CREATE TABLE IF NOT EXISTS `BloodBanks` (`id` int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,`BloodBankName` varchar(255) NOT NULL,`Quantity A+` int(15) NOT NULL,`Quantity A-` int(15) NOT NULL,`Quantity B+` int(15) NOT NULL,`Quantity B-` int(15) NOT NULL,`Quantity O+` int(15) NOT NULL,`Quantity O-` int(15) NOT NULL,`Quantity AB+` int(15) NOT NULL,`Quantity AB-` int(15) NOT NULL)');
+db.query('INSERT INTO `BloodBanks` (`BloodBankName`,`Quantity A+`,`Quantity A-`,`Quantity B+`,`Quantity B-`, `Quantity O+`, `Quantity O-`,`Quantity AB+`,`Quantity AB-`) VALUES ("Charlotte Public Blood Bank",1000,500,1200,800, 300, 100,600,200)');
+db.query('INSERT INTO `BloodBanks` (`BloodBankName`,`Quantity A+`,`Quantity A-`,`Quantity B+`,`Quantity B-`, `Quantity O+`, `Quantity O-`,`Quantity AB+`,`Quantity AB-`) VALUES ("Private Blood Bank 1",1000,500,1200,800, 300, 100,600,200);');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
